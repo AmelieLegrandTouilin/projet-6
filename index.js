a683ecc69344103e64252684ead59b2918786241
Amélie <amelietouilin@gmail.com>
2024-01-03 15:09:27 +0100

    ajout des travaux et du filtre

--- index.js
+++ index.js
@@ -1,38 +1,65 @@
+
+/*travaux dynamiques */
 let works = [];
-const imageElement = document.createElement("img");
 const gallery = document.querySelector(".gallery");
-const figure = document.createElement("figure");
-const img = document.createElement("img");
+const category = [];
 
-function generateWorks (){
-console.log ('affichage données');
-
-for (let i = 0; i < works.length; i++){
-  let image = `<figure>
-  <img src= ${works[i].imageUrl} >
-  <figcaption>${works[i].title}</figcaption>
-  </figure>`;
-  const figure =document.createTextNode(image);
- gallery.appendChild(figure);
- 
-
-console.log (works[i].id);
-
-}
-gallery.innerHTML = galleryContent;
-
-}
-
-
-
-
+/* récupération dynamique du back-end des
+projets de l’architecte. */
 
 async function getWorks() {
-  const response = await fetch ('http://localhost:5678/api/works');
+  const response = await fetch ('http://localhost:5679/api/works');
   works= await response.json();
   
   generateWorks ();
 }
 getWorks();
+function generateWorks (){
+  console.log ('affichage données');
+
+  /* ajout des travaux de maniére dynamique du HTML */
+  
+  for (let i = 0; i < works.length; i++){
+    let image = `<figure>
+    <img src= ${works[i].imageUrl} >
+    <figcaption>${works[i].title}</figcaption>
+    </figure>`;
+    gallery.innerHTML += image;
+  
+  console.log (works[i].id);
+  }
+  }
+
+  /*filtre de la galerie par catégorie de projet */
+  /*récupération du back-end du filtre*/
+  /* ajout du filtre de maniére dynamique du HTML */
+async function getCategories () {
+  const r= await fetch('http://localhost:5679/api/categories')
+  if (r.ok === true){
+    return r.json();
+  }
+  throw new Error ('Impossible de contacter le serveur')
+}
+getCategories().then(categories => {
+  const categoriesContainer = document.querySelector(".categories");
+
+  if (categoriesContainer) {
+    const categoriesHTML = `
+      <ul class="categories-button">
+        <li class="button">Tous</li>
+        <li class="button">Objets</li>
+        <li class="button">Appartements</li>
+        <li class="button">Hôtel & restaurants</li>
+      </ul>
+    `;
+    categoriesContainer.insertAdjacentHTML("beforeend", categoriesHTML);
+  }
+});
+
+  
+
+  
+  
+
 
 
